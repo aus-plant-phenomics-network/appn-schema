3f779b48402f73d1fe769831eeb4b91d486387e8
Donald Hobern <donald.hobern@adelaide.edu.au>
2026-09-10 09:27:10 +1000

    More documentation

--- appn_configuration.py
+++ appn_configuration.py
@@ -11,7 +11,6 @@
 # Created Date: 2026-03-31
 # version ='2026.0.1'
 # -----------------------------------------------------------------------------
-
 import yaml
 import os
 import logging
@@ -20,16 +19,15 @@ from typing import Optional
 from appn_types import NamespaceDefinition, Organisation
 from rdflib import URIRef
 
-CENTRAL_ORGANISATION = "APPN"
-DEFAULT_CENTRAL_VOCABULARY_PREFIX = "appnid"
-
+# Default values for finding YAML configuration file
 APPN_DEFAULT_CONFIGURATION_FOLDER = Path("./")
 APPN_DEFAULT_CONFIGURATION_NAME = "appn"
 
+# Environment keys to access local settings for YAML configuration file
 APPN_CONFIGURATION_FOLDER_ENVIRONMENT_KEY = "APPN_CONFIGURATION_FOLDER"
 APPN_CONFIGURATION_NAME_ENVIRONMENT_KEY = "APPN_CONFIGURATION_NAME"
 
-CONFIGURATION_KEY_NAMESPACES = "namespaces"
+# Keys for elements expected in YAML configuration file
 CONFIGURATION_KEY_NAMESPACE_PATHS = "namespace_paths"
 CONFIGURATION_KEY_VOCABULARY_COLUMN_NAMESPACES = "vocabulary_column_namespaces"
 CONFIGURATION_KEY_EXPLICIT_CLASSES = "explicit_classes"
@@ -42,21 +40,24 @@ CONFIGURATION_KEY_PROPERTY_EXPANSIONS = "property_expansions"
 CONFIGURATION_KEY_EMBEDDED_CLASSES = "embedded_classes"
 CONFIGURATION_KEY_ORGANISATIONS = "organisations"
 
+# Recognised values for namespaces in explicit classes element
 EXPLICIT_CLASSES_ALL = "all"
 EXPLICIT_CLASSES_FIRST = "first"
 
+# Keys for elements in definitions in organisations element
 ORGANISATION_SUBKEY_NAME = "name"
 ORGANISATION_SUBKEY_ROR = "ror"
 ORGANISATION_SUBKEY_NAMESPACE = "namespace"
 ORGANISATION_SUBKEY_PREFIX = "prefix"
 
+# Default settings for central organisation (i.e. APPN)
+CENTRAL_ORGANISATION = "APPN"
+DEFAULT_CENTRAL_VOCABULARY_PREFIX = "appnid"
+
 # Standard APPN namespace URLs
-
 APPN_SCHEMA = "https://schema.plantphenomics.org.au/"
-APPN_VOCABULARY_ROOT = "https://id.plantphenomics.org.au/"
-
-# Schema.org publishes versions using both HTTP and HTTPS - we use
-# HTTPS which seems to be most widely used.
+# NOTE: Schema.org publishes versions using both HTTP and HTTPS - we 
+# use HTTPS which seems to be most widely used.
 SCHEMA_SCHEMA = "https://schema.org/"
 BIO_SCHEMA = "https://bioschemas.org/terms/"
 CDI_SCHEMA = "http://ddialliance.org/Specification/DDI-CDI/1.0/RDF/"
@@ -69,6 +70,8 @@ SKOS_SCHEMA = "http://www.w3.org/2004/02/skos/core#"
 SOSA_SCHEMA = "http://www.w3.org/ns/sosa/"
 SSN_SCHEMA = "http://www.w3.org/ns/ssn/"
 
+# Namespaces for APPN central and node vocabularies
+APPN_VOCABULARY_ROOT = "https://id.plantphenomics.org.au/"
 APPN_VOCABULARY = f"{APPN_VOCABULARY_ROOT}APPN/"
 ANU_VOCABULARY = f"{APPN_VOCABULARY_ROOT}ANU/"
 AU_VOCABULARY = f"{APPN_VOCABULARY_ROOT}AU/"
@@ -80,6 +83,7 @@ USYD_VOCABULARY = f"{APPN_VOCABULARY_ROOT}USYD/"
 UWA_VOCABULARY = f"{APPN_VOCABULARY_ROOT}UWA/"
 WSU_VOCABULARY = f"{APPN_VOCABULARY_ROOT}WSU/"
 
+# Dictionary of default prefixes to use in APPN linked data
 DEFAULT_PREFIXES = {
     APPN_SCHEMA: "appn",
     SCHEMA_SCHEMA: "schema",
@@ -105,22 +109,46 @@ DEFAULT_PREFIXES = {
     WSU_VOCABULARY: "wsu",
 }
 
+### Configuration #############################################################
 
 class Configuration:
+    """
+    Class to access configuration settings for APPN code.
+    
+    Configuration elements are read from a YAML file for which the folder and
+    name can be overridden with environment variables. The default is 
+    "./appn.yaml".
+
+    Access methods always receive a copy of any lists or dictionaries 
+    returned so the configuration is not affected by any changes.
+
+    NOTE: The access methods could be hardened to check the types and 
+    contents of the YAML data.
+    """
 
     def __init__(
         self,
         configuration_folder: Optional[Path | str] = None,
         configuration_name: Optional[str] = None,
     ) -> None:
+        """
+        Read configuration from YAML file
 
+        :param configuration_folder: Folder location containing YAML file
+        :param configuration_name: Name of YAML file (with or without yaml extension)
+        """
+
+        # The severity of errors reported depends on whether an explicit location was
+        # offered or defaults used.
         defaults_overridden = (
             configuration_folder is not None or configuration_name is not None
         )
 
+        # Safe defaults if nothing is read
         self.namespace_definitions = None
         self.configuration = {}
 
+        # Use supplied folder or folder from environment or default
         if configuration_folder is None:
             if APPN_CONFIGURATION_FOLDER_ENVIRONMENT_KEY in os.environ:
                 self.configuration_folder = Path(
@@ -131,18 +159,20 @@ class Configuration:
                 self.configuration_folder = Path(APPN_DEFAULT_CONFIGURATION_FOLDER)
         else:
             self.configuration_folder = Path(configuration_folder)
-
         logging.debug(f"Configuration folder: {self.configuration_folder}")
 
+        # Use supplied filename or filename from environment or default
         if configuration_name is None:
             if APPN_CONFIGURATION_NAME_ENVIRONMENT_KEY in os.environ:
                 configuration_name = os.environ[APPN_CONFIGURATION_NAME_ENVIRONMENT_KEY]
                 defaults_overridden = True
             else:
                 configuration_name = APPN_DEFAULT_CONFIGURATION_NAME
-
+        if not configuration_name.lower().endswith(".yaml"):
+            configuration_name = f"{configuration_name}.yaml"
         logging.debug(f"Configuration name: {configuration_name}")
 
+        # Validate folder
         if not self.configuration_folder.exists():
             message = f"Configuration folder {self.configuration_folder} does not exist"
             if defaults_overridden:
@@ -152,10 +182,8 @@ class Configuration:
                 logging.debug(message)
                 return
 
-        self.configuration_filepath = (
-            self.configuration_folder / f"{configuration_name}.yaml"
-        )
-
+        # Validate file
+        self.configuration_filepath = self.configuration_folder / configuration_name
         if not self.configuration_filepath.exists():
             message = f"Configuration file {self.configuration_filepath} does not exist"
             if defaults_overridden:
@@ -165,6 +193,7 @@ class Configuration:
                 logging.debug(message)
                 return
 
+        # Load configuration from file
         with open(self.configuration_filepath, "r") as stream:
             try:
                 logging.debug(f"Loading configuration: {self.configuration_filepath}")
@@ -176,12 +205,36 @@ class Configuration:
                     f"Failed to load configuration from {self.configuration_filepath}"
                 )
 
+        # Organisations element requires parsing before use - only do this when
+        # requested
         self.organisations = None
 
+        return
+
     def get_namespace_definitions(self) -> dict[str, NamespaceDefinition]:
+        """
+        Return `NamespaceDefinition`s from configuration
+
+        `NamespaceDefinitions` include the namespace, preferred namespace
+        prefix and path for accessing a machine-readable version of the 
+        definitions in the namespace.
+
+        The namespaces and prefixes are defined by `DEFAULT_PREFIXES`. 
+        Any paths are set via the `namespace_paths` key in the YAML
+        configuration.
+
+        :return: Dictionary of `NameDefinition` objects keyed by the 
+            namespace string
+        """
+        # Cache the definitions since these are built from configuration
+        # information combined with defaults where necessary.
         if self.namespace_definitions is not None:
             return self.namespace_definitions.copy()
 
+        # Find any configured namespace paths (locations for schema assets).
+        # Assets without paths will be only be loadable by using the namespace 
+        # as a URL.
+        
         if CONFIGURATION_KEY_NAMESPACE_PATHS in self.configuration and isinstance(
             self.configuration[CONFIGURATION_KEY_NAMESPACE_PATHS], dict
         ):
@@ -190,22 +243,32 @@ class Configuration:
         else:
             paths = {}
 
+        # Build the namespace definitions for all default namespaces, taking
+        # into account the configured namespace paths.
         self.namespace_definitions = {
             ns: NamespaceDefinition(ns, pre, paths[ns] if ns in paths else ns)
             for ns, pre in DEFAULT_PREFIXES.items()
         }
 
-        if CONFIGURATION_KEY_NAMESPACES in self.configuration:
-            definitions = self.configuration[CONFIGURATION_KEY_NAMESPACES]
-            if isinstance(definitions, dict):
-                for ns, pre in definitions.items():
-                    self.namespace_definitions[ns] = NamespaceDefinition(
-                        ns, pre, paths[ns] if ns in paths else ns
-                    )
-
         return self.namespace_definitions.copy()
 
     def get_vocabulary_column_namespaces(self) -> list[str]:
+        """
+        Get list of namespaces to search for mapping spreadsheet columns.
+
+        The `column_namespaces` list controls the eligibility of namespaces
+        to serve as the source of properties matching spreadsheet column 
+        names. The namespaces will be checked in the supplied order to 
+        find a property with a name exactly matching a column heading.
+        Properties from the APPN schema will automatically be checked.
+        Unmatched names will be converted into locally defined properties.
+
+        The list should contain schema.org, SKOS Core and Dublin Core.
+
+        Returns a copy of any list included in the YAML configuration.
+
+        :return: List of namespace strings
+        """
         if CONFIGURATION_KEY_VOCABULARY_COLUMN_NAMESPACES in self.configuration:
             return self.configuration[
                 CONFIGURATION_KEY_VOCABULARY_COLUMN_NAMESPACES
@@ -213,6 +276,32 @@ class Configuration:
         return []
 
     def get_explicit_classes(self) -> dict[str, str | list[str]]:
+        """
+        Get dictionary of additional classes that should explicitly be 
+        specified when applicable
+
+        The APPN schema inherits from classes in several other schemas.
+        Users of the data may benefit if the classes they expect are 
+        explicitly identified via `rdf:type` for all instances of any
+        relevant APPN class.
+
+        The `explicit_classes` list identifies namespaces that should be
+        included as additional `rdf:type` statements. For each such
+        namespace, a list of specific classes for inclusion may be 
+        supplied, or the keyword "all" (indicating any relevant class 
+        from the schema) or the keyword "first" (indicating the class
+        from the schema that is closest in the superclass hierarchy to
+        the APPN class being assigned.
+        
+        The expected behaviour is that, when an instance of an APPN 
+        class is created, the known superclasses of the APPN class should
+        be scanned for classes that match any of these rules, and extra
+        `rdf:type` statements should be added for each match.
+
+        Returns a copy of any dictionary included in the YAML configuration.
+
+        :return: List of namespace strings
+        """
         if CONFIGURATION_KEY_EXPLICIT_CLASSES in self.configuration:
             return self.configuration[CONFIGURATION_KEY_EXPLICIT_CLASSES].copy()
         return {}
--- appn_types.py
+++ appn_types.py
@@ -19,10 +19,17 @@ from rdflib import URIRef
 
 
 ### Organisation ##############################################################
-#
-# Simple class for organisation properties
-#
+
 class Organisation(NamedTuple):
+    """
+    Simple class for organisation properties
+
+    :param id: Short identifier for the organisation
+    :param name: Name of the organisation
+    :param ror: ROR identifier for the organisation
+    :param namespace: Namespace for organisation vocabulary
+    :param prefix: Prefix to be used for namespace in CURIEs
+    """
     id: str
     name: str
     ror: str
@@ -30,20 +37,32 @@ class Organisation(NamedTuple):
     prefix: str
 
 ### NamespaceDefinition #######################################################
-#
-# Simple class for namespace definitions
-#
+
 class NamespaceDefinition(NamedTuple):
+    """
+    Simple class for namespace definitions
+
+    :param namespace: Namespace for a linked-data asset
+    :param prefix: Prefix to be used for namespace
+    :param path: Path to access machine-readable RDF for namespace terms
+    """
     ns: str
     prefix: str
     path: str
 
 
 ### Term ######################################################################
-#
-# Simple class for IRI elements
-#
+
 class Term(NamedTuple):
+    """
+    Simple class for accessing elements based on an IRI string
+
+    :param iri: String representation of the IRI
+    :param curie: String representation of the CURIE form for the IRI
+    :param ns: String representation of the namespace
+    :param prefix: String representation of the prefix used for the namespace
+    :param name: String representation of the unqualified name from the IRI
+    """
     iri: str
     curie: str
     ns: str
@@ -52,20 +71,30 @@ class Term(NamedTuple):
 
 
 ### Triple ####################################################################
-#
-# Simple class for triples of string
-#
+
 class Triple(NamedTuple):
+    """
+    Simple class to represent a triple of strings
+
+    :param subject: String for the subject of a triple
+    :param property: String for the property of a triple
+    :param object: String for the object of a triple
+    """
     subject: str
     property: str
     object: str
 
 
 ### URIRefTriple ##############################################################
-#
-# Simple class for triples of URIRef
-#
+
 class URIRefTriple(NamedTuple):
+    """
+    Simple class to represent a triple of `URIRef`s
+
+    :param subject: `URIRef` for the subject of a triple
+    :param property: `URIRef` for the property of a triple
+    :param object: `URIRef` for the object of a triple
+    """
     subject: URIRef
     property: URIRef
     object: URIRef
